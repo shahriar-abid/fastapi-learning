62166655c1a03fc876a71fe9d9c5cb6663cca389
shahriar-abid <shahriarabid6938@icloud.com>
2026-04-14 21:53:29 +0600

    Built basic student management API with FastAPI (GET, search, sort)

--- main.py
+++ main.py
@@ -1,13 +1,56 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
+import json
 
 app = FastAPI()
 
-@app.get("/")
+def load_data():
+    with open('students.json', 'r') as j:
+        data = json.load(j)
 
+    return data
+
+
+@app.get("/")
 def hello():
-    return {'message' : 'Hello world'}
+    return {'message' : 'Student Management System API'}
+
 @app.get('/about')
 def about():
     return{
-        'message': "CampusX is an education platform where you can learn AI"
+        'message': "A fully functional api for student management"
     }
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
+
+@app.get('/student/{student_id}')
+def view_student(
+    student_id: int = Path(..., description="Id of the student", example=1)
+    ):
+    #load the whole student data
+    data = load_data()
+    for student in data:
+        if student['id'] == student_id:
+            return student
+    raise HTTPException(status_code=404, detail="Student not found")
+
+@app.get('/sort')
+def sort_students(
+    sort_by: str = Query(..., description='sort on the basis of id & cgpa'),
+    order: str = Query('asc', description='sort in acs or desc order')
+    ):
+    valid_fields = ['id', 'cgpa']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail= f'Invalid field, Select from {valid_fields}')
+    if order not in ['asc', "desc"]:
+        raise HTTPException(status_code=400, detail= f'Invalid field, Select between asc and desc')
+    
+    data = load_data()
+    
+    reverse = True if order == 'desc' else False
+    sorted_data = sorted(data, key=lambda x: x[sort_by], reverse=reverse)
+
+    return sorted_data
